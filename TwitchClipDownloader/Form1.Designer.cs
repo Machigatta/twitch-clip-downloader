b1e9349ff69776bf1436574b22756241cc4a379b
Machigatta <germangameguides@gmail.com>
2018-08-05 12:31:18 +0200

    init

---
+++ TwitchClipDownloader/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace TwitchClipDownloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cbGame = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bLookupTop = new System.Windows.Forms.Button();
+            this.ckTrend = new System.Windows.Forms.CheckBox();
+            this.cbPeriod = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rtLog = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pbDownload = new System.Windows.Forms.ProgressBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtSave = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btSave = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtLanguage = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // cbGame
+            // 
+            this.cbGame.FormattingEnabled = true;
+            this.cbGame.Items.AddRange(new object[] {
+            "PLAYERUNKNOWN\'S BATTLEGROUNDS",
+            "Fortnite"});
+            this.cbGame.Location = new System.Drawing.Point(53, 12);
+            this.cbGame.Name = "cbGame";
+            this.cbGame.Size = new System.Drawing.Size(203, 21);
+            this.cbGame.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Game:";
+            // 
+            // bLookupTop
+            // 
+            this.bLookupTop.Location = new System.Drawing.Point(551, 69);
+            this.bLookupTop.Name = "bLookupTop";
+            this.bLookupTop.Size = new System.Drawing.Size(75, 23);
+            this.bLookupTop.TabIndex = 2;
+            this.bLookupTop.Text = "Crawl";
+            this.bLookupTop.UseVisualStyleBackColor = true;
+            this.bLookupTop.Click += new System.EventHandler(this.bLookupTop_Click);
+            // 
+            // ckTrend
+            // 
+            this.ckTrend.AutoSize = true;
+            this.ckTrend.Checked = true;
+            this.ckTrend.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.ckTrend.Location = new System.Drawing.Point(562, 14);
+            this.ckTrend.Name = "ckTrend";
+            this.ckTrend.Size = new System.Drawing.Size(68, 17);
+            this.ckTrend.TabIndex = 3;
+            this.ckTrend.Text = "Trending";
+            this.ckTrend.UseVisualStyleBackColor = true;
+            // 
+            // cbPeriod
+            // 
+            this.cbPeriod.FormattingEnabled = true;
+            this.cbPeriod.Items.AddRange(new object[] {
+            "day",
+            "week",
+            "month",
+            "all"});
+            this.cbPeriod.Location = new System.Drawing.Point(308, 12);
+            this.cbPeriod.Name = "cbPeriod";
+            this.cbPeriod.Size = new System.Drawing.Size(89, 21);
+            this.cbPeriod.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(262, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Period:";
+            // 
+            // rtLog
+            // 
+            this.rtLog.Location = new System.Drawing.Point(15, 111);
+            this.rtLog.Name = "rtLog";
+            this.rtLog.Size = new System.Drawing.Size(611, 129);
+            this.rtLog.TabIndex = 6;
+            this.rtLog.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Log:";
+            // 
+            // pbDownload
+            // 
+            this.pbDownload.Location = new System.Drawing.Point(15, 69);
+            this.pbDownload.Name = "pbDownload";
+            this.pbDownload.Size = new System.Drawing.Size(530, 23);
+            this.pbDownload.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 53);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Download-Progress";
+            // 
+            // txtSave
+            // 
+            this.txtSave.Location = new System.Drawing.Point(12, 259);
+            this.txtSave.Name = "txtSave";
+            this.txtSave.Size = new System.Drawing.Size(533, 20);
+            this.txtSave.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 243);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "SavePath";
+            // 
+            // btSave
+            // 
+            this.btSave.Location = new System.Drawing.Point(551, 259);
+            this.btSave.Name = "btSave";
+            this.btSave.Size = new System.Drawing.Size(75, 23);
+            this.btSave.TabIndex = 12;
+            this.btSave.Text = "Save";
+            this.btSave.UseVisualStyleBackColor = true;
+            this.btSave.Click += new System.EventHandler(this.btSave_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(403, 14);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Language:";
+            // 
+            // txtLanguage
+            // 
+            this.txtLanguage.Location = new System.Drawing.Point(467, 13);
+            this.txtLanguage.Name = "txtLanguage";
+            this.txtLanguage.Size = new System.Drawing.Size(89, 20);
+            this.txtLanguage.TabIndex = 14;
+            this.txtLanguage.Text = "en,de";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(638, 291);
+            this.Controls.Add(this.txtLanguage);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btSave);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtSave);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pbDownload);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.rtLog);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbPeriod);
+            this.Controls.Add(this.ckTrend);
+            this.Controls.Add(this.bLookupTop);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbGame);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Twitch Clip Downloader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbGame;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button bLookupTop;
+        private System.Windows.Forms.CheckBox ckTrend;
+        private System.Windows.Forms.ComboBox cbPeriod;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox rtLog;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar pbDownload;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSave;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btSave;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtLanguage;
+    }
+}
+
---
+++ TwitchClipDownloader/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace TwitchClipDownloader
+{
+    public partial class Form1 : Form
+    {
+        private int downloadCounter = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            cbGame.SelectedIndex = 0;
+            cbPeriod.SelectedIndex = 0;
+            txtSave.Text = Properties.Settings.Default.savePath;
+        }
+
+        private async void bLookupTop_Click(object sender, EventArgs e)
+        {
+            string baseUrl = "https://api.twitch.tv/kraken/clips/top?limit=100";
+            string downloadUri = "";
+
+            //wird nur gecrawled wenn spiel angegeben
+            if (cbGame.Text != "")
+            {
+                baseUrl += "&game=" + cbGame.Text;
+
+                //periode setzen (week ist default)
+                if (cbPeriod.Text != "")
+                {
+                    baseUrl += "&period=" + cbPeriod.Text;
+                }
+
+                //trending ist default auf false
+                if (ckTrend.Checked)
+                {
+                    baseUrl += "&trending=true";
+                }
+
+                //trending ist default auf false
+                if (txtLanguage.Text != "")
+                {
+                    baseUrl += "&language=" + txtLanguage.Text;
+                }
+
+                baseUrl = Uri.EscapeUriString(baseUrl);
+
+                rtLog.Text = "REQUEST TO: " + baseUrl + "\r\n" + rtLog.Text;
+
+                var client = new HttpClient();
+                var request = new HttpRequestMessage()
+                {
+                    RequestUri = new Uri(baseUrl),
+                    Method = HttpMethod.Get,
+                };
+
+                request.Headers.Add("client-id", "h91ejo3shtecvixy0xhcd6mulvitd8");
+                request.Headers.Add("accept", "application/vnd.twitchtv.v5+json");
+                var req = await client.SendAsync(request);
+
+                string testString = await req.Content.ReadAsStringAsync();
+
+                ClipsObject result = (ClipsObject)JsonConvert.DeserializeObject(testString,typeof(ClipsObject));
+                if (result.clips == null)
+                {
+                    rtLog.Text = "Nothing found.\r\n" + rtLog.Text;
+                    return;
+                }
+
+
+                bLookupTop.Enabled = false;
+
+                //each clip -> log it
+                foreach (Clip item in result.clips)
+                {
+
+                    downloadUri = item.thumbnails.medium.Replace("-preview-480x272.jpg", ".mp4");
+                    using (WebClient wc = new WebClient())
+                    {
+
+                        downloadCounter++;
+                        wc.DownloadFileCompleted += Wc_DownloadFileCompleted;
+                        string fileName = Uri.EscapeUriString(item.broadcaster.display_name + "_" + item.slug);
+                        wc.DownloadFileAsync(new System.Uri(@downloadUri),Properties.Settings.Default.savePath.Replace("\\", "\\\\") + fileName + ".mp4");
+
+                        rtLog.Text = "---- " + downloadUri + " ----\r\n" + rtLog.Text;
+                    }
+
+                    
+                }
+
+                rtLog.Text = "---- Starting " + downloadCounter + " Downloads ----\r\n" + rtLog.Text;
+                pbDownload.Maximum = downloadCounter;
+            }
+            else
+            {
+                rtLog.Text = "Kein Spiel angegeben\r\n" + rtLog.Text;
+            }
+        }
+
+        private void Wc_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                rtLog.Text = e.Error.InnerException.Message + "\r\n" + rtLog.Text;
+            }
+            
+            downloadCounter--;
+            pbDownload.Value++;
+
+            if (downloadCounter == 0)
+            {
+                rtLog.Text = "---- All downloads finished ----\r\n" + rtLog.Text;
+                bLookupTop.Enabled = true;
+                pbDownload.Value = 0;
+            }
+            else
+            {
+                
+                rtLog.Text = "Downloads left: " + downloadCounter + "\r\n" + rtLog.Text;
+            }
+            
+        }
+
+        private void btSave_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.savePath = txtSave.Text;
+            Properties.Settings.Default.Save();
+        }
+    }
+      
+}
+
+
+
+
+public class ClipsObject
+{
+    public List<Clip> clips { get; set; }
+}
+
+public class Clip
+{
+    public string slug { get; set; }
+    public broadcaster broadcaster { get; set; }
+    public string title { get; set; }
+    public thumbnails thumbnails { get; set; }
+}
+
+public class thumbnails
+{
+    public string medium { get; set; }
+}
+
+public class broadcaster
+{
+    public string display_name { get; set; }
+}
+
